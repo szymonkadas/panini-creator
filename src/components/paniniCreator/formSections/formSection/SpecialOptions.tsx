8168eb0a31007f11af2f7a13ca072a5b8429e467
Szymon Kadaś <kadasszymon@gmail.com>
2023-10-22 19:43:43 +0200

    fixed bugs

--- src/components/paniniCreator/formSections/formSection/SpecialOptions.tsx
+++ src/components/paniniCreator/formSections/formSection/SpecialOptions.tsx
@@ -1,10 +1,8 @@
 import CheckboxButtonOption from "./options/CheckboxButtonOption";
-import CheckboxOption from "./options/CheckboxOption";
-import RadioOption from "./options/RadioOption";
 import SelectOption from "./options/SelectOption";
 
 type SpecialOptionsProps = {
-  type: "checkboxButton" | "checkbox" | "radio" | "select";
+  type: "checkboxButton" | "select";
   options: string[];
 };
 
@@ -13,12 +11,6 @@ export default function SpecialOptions(props: SpecialOptionsProps) {
     switch (props.type) {
       case "checkboxButton":
         return <CheckboxButtonOption {...{ option, index }} />;
-        break;
-      case "checkbox":
-        return <CheckboxOption {...{ option, index }} />;
-        break;
-      case "radio":
-        return <RadioOption {...{ option, index }} />;
         break;
       case "select":
         return <SelectOption {...{ option, index }} />;
