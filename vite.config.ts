8ccca77911be61348068d66062595bb3739a585f
Szymon Kadaś <kadasszymon@gmail.com>
2023-11-10 20:03:22 +0100

    deployment preparation

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,5 @@ export default defineConfig({
     "process.env.VITE_APP_API_KEY": JSON.stringify(process.env.VITE_APP_API_KEY),
     "process.env.VITE_APP_API_URL": JSON.stringify(process.env.VITE_APP_API_URL),
   },
+  base: "/panini-creator/",
 });
