3e6d205e14fdc97504e632147026672a28ecdcda
Szymon Kadaś <KadasSzymon@gmail.com>
2025-09-29 11:57:05 +0200

    Deployed (#28)
    * deployment preparation
    
    * fixed dices bug
    
    * fixed dices bug, also changed paths of images to public ones.
    
    * changed undeployed images to components
    
    * chore: azure deploy config

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,43 @@
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./pages/Layout";
 import PaniniCreator from "./pages/PaniniCreator";
 import SplashScreenLayout from "./pages/SplashScreenLayout";
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Layout />}>
-        <Route
-          index
-          element={
-            <SplashScreenLayout
-              shouldTransition={false}
-              defaultPos={true}
-              navTo="/panini_creator"
-              title="Panini Creator"
-              actionDesc="begin"
-            />
-          }
-        />
-        <Route path="/panini_creator" element={<PaniniCreator navTo="/success" />}></Route>
-        <Route
-          path="/success"
-          element={
-            <SplashScreenLayout
-              shouldTransition={true}
-              defaultPos={false}
-              navTo="/panini_creator"
-              title="Panini ordered"
-              actionDesc="start again"
-            ></SplashScreenLayout>
-          }
-        ></Route>
-      </Route>
-    )
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={
+              <SplashScreenLayout
+                shouldTransition={false}
+                defaultPos={true}
+                navTo="/panini_creator"
+                title="Panini Creator"
+                actionDesc="begin"
+              />
+            }
+          />
+          <Route path="/panini_creator" element={<PaniniCreator navTo="/success" />} />
+          <Route
+            path="/success"
+            element={
+              <SplashScreenLayout
+                shouldTransition={true}
+                defaultPos={false}
+                navTo="/panini_creator"
+                title="Panini ordered"
+                actionDesc="start again"
+              />
+            }
+          />
+        </Route>
+      </Routes>
+    </HashRouter>
   );
-  return <RouterProvider router={router} />;
 }
 
 export default App;
